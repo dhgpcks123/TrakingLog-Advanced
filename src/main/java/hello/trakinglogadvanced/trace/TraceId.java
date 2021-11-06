ca08b3efdb3159bb84a30790ddc9cf0050ec4d38
hyechan <dhgpcks123@naver.com>
2021-11-06 21:42:02 +0900

    로그 추적기 level, prefix추가

--- src/main/java/hello/trakinglogadvanced/trace/TraceId.java
+++ src/main/java/hello/trakinglogadvanced/trace/TraceId.java
@@ -12,23 +12,24 @@ public class TraceId {
         this.level = 0;
     }
 
-    private TraceId(String id, int level){
+    private TraceId(String id, int level) {
         this.id = id;
         this.level = level;
     }
 
-    private String createId(){
+    private String createId() {
         return UUID.randomUUID().toString().substring(0, 8);
     }
 
-    public TraceId createNextId(){
+    public TraceId createNextId() {
         return new TraceId(id, level + 1);
     }
 
-    public TraceId createPreviousId(){
+    public TraceId createPreviousId() {
         return new TraceId(id, level - 1);
     }
-    public boolean isFirstLevel(){
+
+    public boolean isFirstLevel() {
         return level == 0;
     }
 
--- src/main/java/hello/trakinglogadvanced/trace/hellotrace/HelloTraceV2.java
+++ src/main/java/hello/trakinglogadvanced/trace/hellotrace/HelloTraceV2.java
@@ -7,21 +7,29 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class HelloTraceV1 {
+public class HelloTraceV2 {
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
-
-    public TraceStatus begin(String message){
+    public TraceStatus begin(String message) {
         TraceId traceId = new TraceId();
-        long startTimeMs = System.currentTimeMillis();
-        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[" + traceId.getId() + "] " + addSpace(START_PREFIX,
+                traceId.getLevel()) + message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
-    public void end(TraceStatus status){
+    //V2에서 추가
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX, nextId.getLevel()) + message);
+        return new TraceStatus(nextId, startTimeMs, message);
+    }
+    public void end(TraceStatus status) {
         complete(status, null);
     }
+
     public void exception(TraceStatus status, Exception e){
         complete(status, e);
     }
@@ -31,21 +39,16 @@ public class HelloTraceV1 {
         long resultTimeMs = stopTimeMs - status.getStartTimeMs();
         TraceId traceId = status.getTraceId();
         if(e == null){
-            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+            log.info("[" + traceId.getId() + "] " + addSpace(COMPLETE_PREFIX, traceId.getLevel()) + status.getMessage() + " time=" + resultTimeMs + "ms");
         }else {
-            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+            log.info("[" + traceId.getId() + "] " + addSpace(EX_PREFIX, traceId.getLevel()) + status.getMessage() + " time=" + resultTimeMs + "ms" + " ex=" + e);
         }
     }
 
-    // level=0
-    // level=1 |-->
-    // level=2 |   |-->
-    // level=2 ex  |   |<X-
-    // level=1 ex  |<X-
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
-            sb.append( (i == level - 1) ? "|" + prefix : "|   ");
+            sb.append( (i == level - 1) ? "---|" + prefix : "----");
         }
         return sb.toString();
     }
