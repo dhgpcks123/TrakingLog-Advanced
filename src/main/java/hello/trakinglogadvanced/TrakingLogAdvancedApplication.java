497c5957757fb2a641e8a2baf8b4ddc072f40a0e
hyechan <dhgpcks123@naver.com>
2021-11-06 16:58:43 +0900

    git init

---
+++ src/main/java/hello/trakinglogadvanced/TrakingLogAdvancedApplication.java
@@ -0,0 +1,13 @@
+package hello.trakinglogadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrakingLogAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrakingLogAdvancedApplication.class, args);
+    }
+
+}
